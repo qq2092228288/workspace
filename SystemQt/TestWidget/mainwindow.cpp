643aa4407458d327acea713fca714cc4cac2aac5
qq2092228288 <2092228288@qq.com>
2023-04-17 15:42:35 +0800

    commonFiles  qmqtt

---
+++ SystemQt/TestWidget/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    qDebug()<<QT_VERSION_STR;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
